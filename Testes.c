fb3c03305182707b22aecc4f8da1d9ad8b72cb55
JoaoSiberi <joaogabrielsiberi3@gmail.com>
2025-04-04 20:47:42 +0000

    Testes encadeados e aninhados

--- Testes.c
+++ Testes.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+
+int main (){
+
+    int numero;
+
+    printf("Digite um número:\n");
+    scanf("%d",&numero);
+
+    int resultado = numero % 2;
+
+   if (numero > 0)
+   {
+    if (numero % 2 == 0)
+    {
+        printf("O número é positivo e par\n");
+    }
+    else {
+        printf("O número é positivo e impar\nn");
+    }
+   }
+   else if (numero == 0)
+   {
+    printf("O número é 0\n");
+}else {
+    printf("Negativo\n");
+}
+  
+}
+
+
+
+
+
+
 
